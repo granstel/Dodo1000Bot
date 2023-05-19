ce086145675e9000e92b1b509f02a83df1db34d8
Stepan Grankin <sgrankin@inbox.ru>
2023-05-19 23:56:14 +0300

    HttpClientExtensions

---
+++ Dodo1000Bot.Services/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,25 @@
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Dodo1000Bot.Services.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<TResult> GetAsync<TResult>(this HttpClient httpClient, string url,
+            JsonSerializerSettings serializerOptions, CancellationToken cancellationToken)
+        {
+            url = url.ToLower();
+
+            var response = await httpClient.GetAsync(url, cancellationToken);
+
+            response.EnsureSuccessStatusCode();
+            var allUnitsString = await response.Content.ReadAsStringAsync(/*cancellationToken*/);
+
+            var result = JsonConvert.DeserializeObject<TResult>(allUnitsString, serializerOptions);
+
+            return result;
+        }
+    }
+}
