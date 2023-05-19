aa599aaf5b53de2d3b9f71a12ebe75e734c7a878
Stepan Grankin <sgrankin@inbox.ru>
2023-05-19 23:47:11 +0300

    IGlobalApiClient

---
+++ Dodo1000Bot.Services/Interfaces/IGlobalApiClient.cs
@@ -0,0 +1,11 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Dodo1000Bot.Models.GlobalApi;
+
+namespace Dodo1000Bot.Services
+{
+    public interface IGlobalApiClient
+    {
+        Task<BrandListTotalUnitCountListModel> UnitsCount(CancellationToken cancellationToken);
+    }
+}
