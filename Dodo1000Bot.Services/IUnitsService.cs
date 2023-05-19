71557f2fb5bd0bd2850b58d6f2d16a0fa6cef730
Stepan Grankin <sgrankin@inbox.ru>
2023-05-19 23:31:53 +0300

    CT

--- Dodo1000Bot.Services/IUnitsService.cs
+++ Dodo1000Bot.Services/IUnitsService.cs
@@ -1,9 +1,10 @@
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Dodo1000Bot.Services
 {
     public interface IUnitsService
     {
-        Task CheckAndNotify();
+        Task CheckAndNotify(CancellationToken cancellationToken);
     }
 }
