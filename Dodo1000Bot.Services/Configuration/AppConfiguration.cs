6b037c5d0b4a0e6db77666eabde0baa3ec58fff9
Stepan Grankin <sgrankin@inbox.ru>
2023-05-19 23:31:43 +0300

    UnitsConfiguration at AppConfiguration

--- Dodo1000Bot.Services/Configuration/AppConfiguration.cs
+++ Dodo1000Bot.Services/Configuration/AppConfiguration.cs
@@ -7,5 +7,7 @@
         public DialogflowConfiguration Dialogflow { get; set; }
 
         public RedisConfiguration Redis { get; set; }
+
+        public UnitsConfiguration Units { get; set; }
     }
 }
