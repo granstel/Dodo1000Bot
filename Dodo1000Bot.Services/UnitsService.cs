f42ee484701e133fcd69ae169dfa20cdbe42bf68
Stepan Grankin <sgrankin@inbox.ru>
2023-05-24 21:43:20 +0300

    AboutTotalAtCountries

--- Dodo1000Bot.Services/UnitsService.cs
+++ Dodo1000Bot.Services/UnitsService.cs
@@ -20,22 +20,41 @@ public class UnitsService : IUnitsService
     {
         var unitsCount = await _globalApiClient.UnitsCount(cancellationToken);
 
-        await CheckAndNotifyAboutTotalAtBrands(unitsCount, cancellationToken);
+        await AboutTotalAtBrands(unitsCount, cancellationToken);
     }
 
-    private async Task CheckAndNotifyAboutTotalAtBrands(BrandListTotalUnitCountListModel unitsCount, CancellationToken cancellationToken)
+    private async Task AboutTotalAtBrands(BrandListTotalUnitCountListModel unitsCount, CancellationToken cancellationToken)
     {
         var totalAtBrands = unitsCount.Brands.ToDictionary(b => b.Brand, b => b.Total);
 
         foreach (var totalAtBrand in totalAtBrands)
         {
-            if (totalAtBrand.Value % 1000 == 0)
+            if (totalAtBrand.Value % 1000 != 0)
             {
                 continue;
             }
 
-            var notification = $"There is {totalAtBrand.Value} units at {totalAtBrand.Key}";
+            var notification = $"There is {totalAtBrand.Value} units of {totalAtBrand.Key} brand";
             await _notifyService.Notify(notification, cancellationToken);
+        }
+    }
+
+    private async Task AboutTotalAtCountries(BrandListTotalUnitCountListModel unitsCount, CancellationToken cancellationToken)
+    {
+        var totalAtBrandAtCountries = unitsCount.Brands.ToDictionary(b => b.Brand, b => b.Countries.ToDictionary(c => c.CountryName, c => c.PizzeriaCount));
+
+        foreach (var totalAtBrandAtCountry in totalAtBrandAtCountries)
+        {
+            foreach (var totalAtCountry in totalAtBrandAtCountry.Value)
+            {
+                if (totalAtCountry.Value % 1000 != 0)
+                {
+                    continue;
+                }
+
+                var notification = $"There is {totalAtCountry.Value} units of {totalAtBrandAtCountry.Key} at {totalAtCountry.Key}";
+                await _notifyService.Notify(notification, cancellationToken);
+            }
         }
     }
 }
