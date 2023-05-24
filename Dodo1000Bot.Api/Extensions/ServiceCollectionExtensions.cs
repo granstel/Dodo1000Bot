cbae86db43f84d0a4d86c521e7f9f9d0e6485d48
Stepan Grankin <sgrankin@inbox.ru>
2023-05-24 21:53:20 +0300

    ServiceCollectionExtensions

---
+++ Dodo1000Bot.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Prometheus;
+
+namespace Dodo1000Bot.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddHttpClient<TClient, TImplementation>(this IServiceCollection services, string endpointUrl,
+        string endpointName)where TClient : class
+        where TImplementation : class, TClient
+    {
+        services.AddHttpClient<TClient, TImplementation>(c =>
+        {
+            if (!Uri.TryCreate(endpointUrl, UriKind.Absolute, out Uri result))
+            {
+                throw new ArgumentException($"Can't create Uri from {endpointName}. " +
+                                            $"Value: {endpointUrl}");
+            }
+
+            c.BaseAddress = result;
+        }).UseHttpClientMetrics();
+    }
+}
