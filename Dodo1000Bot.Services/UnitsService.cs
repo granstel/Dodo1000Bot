956999b6aba8eae3b23d2e7738cefddbb501ba34
Stepan Grankin <sgrankin@inbox.ru>
2023-05-24 21:26:58 +0300

    UnitsService

---
+++ Dodo1000Bot.Services/UnitsService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dodo1000Bot.Services;
+
+public class UnitsService : IUnitsService
+{
+    private IGlobalApiClient _globalApiClient;
+
+    public UnitsService(IGlobalApiClient globalApiClient)
+    {
+        _globalApiClient = globalApiClient;
+    }
+
+    public async Task CheckAndNotify(CancellationToken cancellationToken)
+    {
+        var unitsCount = await _globalApiClient.UnitsCount(cancellationToken);
+    }
+}
