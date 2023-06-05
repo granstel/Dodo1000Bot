1721642fe3746a3d0db1913f806192db54582323
Stepan Grankin <sgrankin@inbox.ru>
2023-06-05 22:35:59 +0300

    AddConstraint_pushed_notifications_notificationId_notifications_Id migration

---
+++ Dodo1000Bot.Api/Migrations/202306052223_AddConstraint_pushed_notifications_notificationId_notifications_Id.cs
@@ -0,0 +1,25 @@
+﻿using FluentMigrator;
+
+namespace Dodo1000Bot.Api.Migrations;
+
+[Migration(202306052223)]
+public class AddConstraint_pushed_notifications_notificationId_notifications_Id: Migration
+{
+    public override void Up()
+    {
+        Execute.Sql
+        (@"
+            ALTER TABLE pushed_notifications
+            ADD CONSTRAINT `pushed_notifications_notificationId_notifications_Id`
+              FOREIGN KEY (`NotificationId`)
+              REFERENCES notifications (`Id`)
+              ON DELETE NO ACTION
+              ON UPDATE NO ACTION;
+        ");
+    }
+
+    public override void Down()
+    {
+        throw new System.NotImplementedException();
+    }
+}
