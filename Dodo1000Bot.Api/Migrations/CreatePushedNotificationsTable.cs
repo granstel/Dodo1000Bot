9aeb078254aa00d693aead2cf89ce3052c593ed7
Stepan Grankin <sgrankin@inbox.ru>
2023-06-05 22:17:00 +0300

    CreatePushedNotificationsTable migration

---
+++ Dodo1000Bot.Api/Migrations/CreatePushedNotificationsTable.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace Dodo1000Bot.Api.Migrations;
+
+[Migration(202306052156)]
+public class CreatePushedNotificationsTable: Migration
+{
+    public override void Up()
+    {
+        Execute.Sql
+        (@"
+            CREATE TABLE IF NOT EXISTS pushed_notifications (
+              `Id` INT NOT NULL AUTO_INCREMENT,
+              `NotificationId` INT NULL,
+              `PushedAt` DATETIME NOT NULL,
+              `UserId` INT NOT NULL,
+              PRIMARY KEY (`Id`),
+              UNIQUE INDEX `Id_UNIQUE` (`Id` ASC) VISIBLE);
+        ");
+    }
+
+    public override void Down()
+    {
+        throw new System.NotImplementedException();
+    }
+}
