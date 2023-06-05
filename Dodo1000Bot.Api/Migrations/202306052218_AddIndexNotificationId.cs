7d49052d5897023ea44809e7572d8cc3b96602ed
Stepan Grankin <sgrankin@inbox.ru>
2023-06-05 22:22:45 +0300

    AddIndexNotificationId migration

---
+++ Dodo1000Bot.Api/Migrations/202306052218_AddIndexNotificationId.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Dodo1000Bot.Api.Migrations;
+
+[Migration(202306052218)]
+public class AddIndexNotificationId: Migration
+{
+    public override void Up()
+    {
+        Execute.Sql
+        (@"
+            ALTER TABLE pushed_notifications 
+                ADD INDEX `NotificationsId` (`NotificationId` ASC) VISIBLE;
+        ");
+    }
+
+    public override void Down()
+    {
+        throw new System.NotImplementedException();
+    }
+}
