4c476121d6074fc8d2aea7ec3590de11748c966e
Stepan Grankin <sgrankin@inbox.ru>
2023-06-06 21:18:57 +0300

    removed redundant

--- Dodo1000Bot.Api/DependencyModules/DatabaseRegistration.cs
+++ Dodo1000Bot.Api/DependencyModules/DatabaseRegistration.cs
@@ -2,7 +2,6 @@
 using Dodo1000Bot.Services.Configuration;
 using FluentMigrator.Runner;
 using Microsoft.Extensions.DependencyInjection;
-using MySql.Data.MySqlClient;
 
 namespace Dodo1000Bot.Api.DependencyModules;
 
@@ -10,8 +9,6 @@ public static class DatabaseRegistration
 {
     internal static void AddMigrations(this IServiceCollection services, AppConfiguration configuration)
     {
-        services.AddSingleton(_ => new MySqlConnection(configuration.MysqlConnectionString));
-
         services.AddFluentMigratorCore()
             .ConfigureRunner(rb =>
             {
