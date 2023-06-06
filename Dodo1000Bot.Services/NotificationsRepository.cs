0b3df4cd6a6694c29d7be04e7f91f356588a26fa
Stepan Grankin <sgrankin@inbox.ru>
2023-06-06 21:57:24 +0300

    NotificationsRepository

---
+++ Dodo1000Bot.Services/NotificationsRepository.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace Dodo1000Bot.Services;
+
+public class NotificationsRepository
+{
+    private readonly MySqlConnection _connection;
+
+    public NotificationsRepository(MySqlConnection connection)
+    {
+        _connection = connection;
+    }
+
+    public async Task Save(string notification)
+    {
+        await _connection.ExecuteAsync(
+            "INSERT INTO notification (payload) VALUES (@payload)",
+            new
+            {
+                payload = notification
+            });
+    }
+}
