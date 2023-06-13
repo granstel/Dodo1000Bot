af9ba9fa438cb7816eb2ade7f040cb9a9d3ecfb6
Stepan Grankin <sgrankin@inbox.ru>
2023-06-13 19:19:35 +0300

    Save Notification

--- Dodo1000Bot.Services/Interfaces/INotifyService.cs
+++ Dodo1000Bot.Services/Interfaces/INotifyService.cs
@@ -1,9 +1,10 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Dodo1000Bot.Models;
 
 namespace Dodo1000Bot.Services;
 
 public interface INotifyService
 {
-    Task Notify(string notification, CancellationToken cancellationToken);
+    Task Notify(Notification notification, CancellationToken cancellationToken);
 }
--- Dodo1000Bot.Services/NotifyService.cs
+++ Dodo1000Bot.Services/NotifyService.cs
@@ -1,12 +1,20 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
+using Dodo1000Bot.Models;
 
 namespace Dodo1000Bot.Services;
 
 public class NotifyService : INotifyService
 {
-    public Task Notify(string notification, CancellationToken cancellationToken)
+    private readonly INotificationsRepository _notificationsRepository;
+
+    public NotifyService(INotificationsRepository notificationsRepository)
     {
-        return Task.CompletedTask;
+        _notificationsRepository = notificationsRepository;
+    }
+
+    public Task Notify(Notification notification, CancellationToken cancellationToken)
+    {
+        return _notificationsRepository.Save(notification, cancellationToken);
     }
 }
