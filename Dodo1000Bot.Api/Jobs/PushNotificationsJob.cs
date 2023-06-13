b3e1e1429b6abd078eee803e5db2b3f40478d151
Stepan Grankin <sgrankin@inbox.ru>
2023-06-13 21:28:14 +0300

    PushNotificationsJob

---
+++ Dodo1000Bot.Api/Jobs/PushNotificationsJob.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Dodo1000Bot.Services;
+using Microsoft.Extensions.Logging;
+
+namespace Dodo1000Bot.Api.Jobs;
+
+public class PushNotificationsJob: RepeatableJob
+{
+    private readonly INotificationsService _notificationsService;
+    public PushNotificationsJob(ILogger<PushNotificationsJob> log, TimeSpan repeatEveryTime, INotificationsService notificationsService) : base(log, repeatEveryTime)
+    {
+        _notificationsService = notificationsService;
+    }
+
+    protected override async Task Execute(CancellationToken cancellationToken)
+    {
+        await _notificationsService.PushNotifications(cancellationToken);
+    }
+}
