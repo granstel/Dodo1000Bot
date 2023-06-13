d04d31b1b3e754bba4314984d353e68d164b1485
Stepan Grankin <sgrankin@inbox.ru>
2023-06-13 22:02:34 +0300

    TelegramNotifyService

---
+++ Dodo1000Bot.Services/INotifyService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Dodo1000Bot.Models;
+
+namespace Dodo1000Bot.Services;
+
+public interface INotifyService
+{
+    public Task NotifyAbout(Notification notification, CancellationToken cancellationToken);
+
+    Task NotifyAbout(IEnumerable<Notification> notifications, CancellationToken cancellationToken);
+}
