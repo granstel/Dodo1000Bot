156d2be89e9c66e197047827abffa9f5fcdc4ad2
Stepan Grankin <sgrankin@inbox.ru>
2023-06-14 22:29:41 +0300

    scoped INotifyService

--- Dodo1000Bot.Api/Jobs/PushNotificationsJob.cs
+++ Dodo1000Bot.Api/Jobs/PushNotificationsJob.cs
@@ -1,8 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Dodo1000Bot.Services;
 using Dodo1000Bot.Services.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Dodo1000Bot.Api.Jobs;
@@ -11,19 +13,24 @@ public class PushNotificationsJob: RepeatableJob
 {
     private readonly INotificationsService _notificationsService;
     private readonly IEnumerable<INotifyService> _notifyServices;
+    private readonly IServiceProvider _provider;
 
     public PushNotificationsJob(
-        ILogger<PushNotificationsJob> log, 
+        ILogger<PushNotificationsJob> log,
         INotificationsService notificationsService,
         IEnumerable<INotifyService> notifyServices,
-        PushNotificationsConfiguration configuration) : base(log, configuration.EveryTime)
+        PushNotificationsConfiguration configuration,
+        IServiceProvider provider) : base(log, configuration.EveryTime)
     {
         _notificationsService = notificationsService;
         _notifyServices = notifyServices;
+        _provider = provider;
     }
 
     protected override async Task Execute(CancellationToken cancellationToken)
     {
+        await using var scope = _provider.CreateAsyncScope();
+        var notifyServices = scope.ServiceProvider.GetServices<INotifyService>();
         await _notificationsService.PushNotifications(_notifyServices, cancellationToken);
     }
 }
