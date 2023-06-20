72b8b7dd184e7c812c1820e7ec968b2070e4188c
Stepan Grankin <sgrankin@inbox.ru>
2023-06-20 21:11:10 +0300

    NotifyAbout_NoAnyNotifications_NothingHappened

--- Dodo1000Bot.Messengers.Telegram.Tests/TelegramNotifyServiceTests.cs
+++ Dodo1000Bot.Messengers.Telegram.Tests/TelegramNotifyServiceTests.cs
@@ -1,8 +1,14 @@
 ﻿using AutoFixture;
+using Dodo1000Bot.Models;
+using Dodo1000Bot.Models.Domain;
 using Dodo1000Bot.Services;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Telegram.Bot;
 
 namespace Dodo1000Bot.Messengers.Telegram.Tests
@@ -32,6 +38,27 @@ namespace Dodo1000Bot.Messengers.Telegram.Tests
             _target = new TelegramNotifyService(_usersRepositoryMock.Object, _clientMock.Object, _logMock);
 
             _fixture = new Fixture();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _mockRepository.VerifyAll();
+            _mockRepository.VerifyNoOtherCalls();
+        }
+
+        [Test]
+        public async Task NotifyAbout_NoAnyNotifications_NothingHappened()
+        {
+            var notifications = Enumerable.Empty<Notification>();
+
+            var ct = CancellationToken.None;
+
+            _usersRepositoryMock.Setup(r => r.GetUsers(Source.Telegram, ct)).ReturnsAsync(() => null);
+
+            var result = await _target.NotifyAbout(notifications, ct);
+
+            Assert.IsEmpty(result);
         }
     }
 }
