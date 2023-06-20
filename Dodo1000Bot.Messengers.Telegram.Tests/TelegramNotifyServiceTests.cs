8a0a7322b5ef1421f57f6c8e861512f16f8bd9f9
Stepan Grankin <sgrankin@inbox.ru>
2023-06-20 21:04:31 +0300

    TelegramNotifyServiceTests

---
+++ Dodo1000Bot.Messengers.Telegram.Tests/TelegramNotifyServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using AutoFixture;
+using Dodo1000Bot.Services;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using Telegram.Bot;
+
+namespace Dodo1000Bot.Messengers.Telegram.Tests
+{
+    [TestFixture]
+    public class TelegramNotifyServiceTests
+    {
+        private MockRepository _mockRepository;
+
+        private Mock<IUsersRepository> _usersRepositoryMock;
+        private Mock<ITelegramBotClient> _clientMock;
+        private ILogger<TelegramNotifyService> _logMock;
+
+        private TelegramNotifyService _target;
+
+        private Fixture _fixture;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockRepository = new MockRepository(MockBehavior.Strict);
+
+            _usersRepositoryMock = _mockRepository.Create<IUsersRepository>();
+            _clientMock = _mockRepository.Create<ITelegramBotClient>();
+            _logMock = Mock.Of<ILogger<TelegramNotifyService>>();
+
+            _target = new TelegramNotifyService(_usersRepositoryMock.Object, _clientMock.Object, _logMock);
+
+            _fixture = new Fixture();
+        }
+    }
+}
