3caeec4164d89bb08f0577314336e953cd0bf1be
Stepan Grankin <sgrankin@inbox.ru>
2023-07-01 22:40:38 +0300

    unique_messengeruserid_messengertype

---
+++ Dodo1000Bot.Api/Migrations/202307012236_AddUnique_MessengerUserId_MessengerType.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Dodo1000Bot.Api.Migrations;
+
+[Migration(202307012236)]
+public class AddUnique_MessengerUserId_MessengerType: Migration
+{
+    public override void Up()
+    {
+        Execute.Sql
+        (@"
+            ALTER TABLE users 
+            ADD UNIQUE INDEX `unique_messengeruserid_messengertype` (`MessengerUserId` ASC, `MessengerType` ASC) INVISIBLE;
+        ");
+    }
+
+    public override void Down()
+    {
+        throw new System.NotImplementedException();
+    }
+}
