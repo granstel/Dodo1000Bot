230ba40bc21a4b01e2823d6dbc7680096d6b8d54
Stepan Grankin <sgrankin@inbox.ru>
2023-07-21 20:23:35 +0300

    nullable HappenedAt

--- Dodo1000Bot.Models/Domain/NotificationPayload.cs
+++ Dodo1000Bot.Models/Domain/NotificationPayload.cs
@@ -1,12 +1,16 @@
-﻿namespace Dodo1000Bot.Models.Domain
+﻿using System;
+
+namespace Dodo1000Bot.Models.Domain
 {
     public class NotificationPayload
     {
         public string Text { get; init; }
 
+        public DateTime? HappenedAt { get; init; }
+
         public override string ToString()
         {
-            return Text;
+            return $"{HappenedAt:d}:{Text}";
         }
     }
 }
