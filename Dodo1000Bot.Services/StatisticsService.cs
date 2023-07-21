7e153c828c1da490706bdd64a1550abbe39a5365
Stepan Grankin <sgrankin@inbox.ru>
2023-07-21 20:32:58 +0300

    StatisticsService

---
+++ Dodo1000Bot.Services/StatisticsService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Dodo1000Bot.Models.Domain;
+using Dodo1000Bot.Models.RealtimeBoard;
+
+namespace Dodo1000Bot.Services;
+
+public class StatisticsService : ICheckAndNotifyService
+{
+    private readonly IRealtimeBoardApiClient _realtimeBoardApiClient;
+    private readonly INotificationsService _notificationsService;
+
+    public StatisticsService(IRealtimeBoardApiClient realtimeBoardApiClient, INotificationsService notificationsService)
+    {
+        _realtimeBoardApiClient = realtimeBoardApiClient;
+        _notificationsService = notificationsService;
+    }
+
+    public async Task CheckAndNotify(CancellationToken cancellationToken)
+    {
+        var statistics = await _realtimeBoardApiClient.Statistics(cancellationToken);
+
+        await AboutOrdersPerMinute(statistics, cancellationToken);
+        await AboutYearRevenue(statistics, cancellationToken);
+    }
+
+    private async Task AboutOrdersPerMinute(Statistics unitsCount, CancellationToken cancellationToken)
+    {
+        var ordersPerMinute = unitsCount.OrdersPerMinute;
+
+        if (!CheckTheRule(ordersPerMinute))
+        {
+            return;
+        }
+
+        var notification = new Notification
+        {
+            Payload = new NotificationPayload
+            {
+                Text = $"There is {ordersPerMinute} orders per minute! See that on https://realtime.dodobrands.io",
+                HappenedAt = DateTime.Now
+            }
+        };
+        await _notificationsService.Save(notification, cancellationToken);
+    }
+
+    private async Task AboutYearRevenue(Statistics unitsCount, CancellationToken cancellationToken)
+    {
+        var yearRevenue = unitsCount.Revenues.Where(r => r.Type == RevenueTypes.Year).Select(r => r.Revenue).FirstOrDefault();
+
+        if (!CheckTheRule(yearRevenue))
+        {
+            return;
+        }
+
+        var notification = new Notification
+        {
+            Payload = new NotificationPayload
+            {
+                Text = $"There is {yearRevenue} dollars in revenue this year!"
+            }
+        };
+        await _notificationsService.Save(notification, cancellationToken);
+    }
+
+    private bool CheckTheRule(int value)
+    {
+        return value % 1000 == 0;
+    }
+
+    private bool CheckTheRule(decimal value)
+    {
+        return value % 1000 == 0;
+    }
+}
