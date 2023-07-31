bd425cbd666e11616466a2a0c4a6ad14aa8c348d
Stepan Grankin <sgrankin@inbox.ru>
2023-07-31 20:32:14 +0300

    CreateSnapshotsTable migration

---
+++ Dodo1000Bot.Api/Migrations/202307312023_CreateSnapshotsTable.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+
+namespace Dodo1000Bot.Api.Migrations;
+
+[Migration(202307312023)]
+public class CreateSnapshotsTable: Migration
+{
+    public override void Up()
+    {
+        Create.Table("payloads")
+            .WithColumn("Id").AsInt32().NotNullable().PrimaryKey()
+            .WithColumn("Name").AsString().NotNullable().Unique()
+            .WithColumn("data").AsCustom("json").NotNullable();
+    }
+
+    public override void Down()
+    {
+        Delete.Table("payloads");
+    }
+}
