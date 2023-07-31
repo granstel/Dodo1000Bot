42f9652b1222963bb1cc82c8e02fc90797234400
Stepan Grankin <sgrankin@inbox.ru>
2023-07-31 21:01:44 +0300

    get  and update snapshot

--- Dodo1000Bot.Services/UnitsService.cs
+++ Dodo1000Bot.Services/UnitsService.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Dodo1000Bot.Models;
 using Dodo1000Bot.Models.Domain;
 using Dodo1000Bot.Models.GlobalApi;
+using Dodo1000Bot.Services.Interfaces;
 using Microsoft.Extensions.Logging;
 
 [assembly: InternalsVisibleTo("Dodo1000Bot.Services.Tests")]
@@ -18,12 +19,18 @@ public class UnitsService : CheckAndNotifyService
     private readonly ILogger<UnitsService> _log;
     private readonly IGlobalApiClient _globalApiClient;
     private readonly INotificationsService _notificationsService;
+    private readonly ISnapshotsRepository _snapshotsRepository;
 
-    public UnitsService(ILogger<UnitsService> log, IGlobalApiClient globalApiClient, INotificationsService notificationsService)
+    public UnitsService(
+        ILogger<UnitsService> log, 
+        IGlobalApiClient globalApiClient, 
+        INotificationsService notificationsService, 
+        ISnapshotsRepository snapshotsRepository)
     {
         _log = log;
         _globalApiClient = globalApiClient;
         _notificationsService = notificationsService;
+        _snapshotsRepository = snapshotsRepository;
     }
 
     public override async Task CheckAndNotify(CancellationToken cancellationToken)
@@ -32,14 +39,27 @@ public class UnitsService : CheckAndNotifyService
         {
             var unitsCount = await _globalApiClient.UnitsCount(cancellationToken);
 
+            var snapshotName = nameof(_globalApiClient.UnitsCount);
+            var unitsCountSnapshot = 
+                await _snapshotsRepository.Get<BrandListTotalUnitCountListModel>(snapshotName, cancellationToken);
+
             await AboutTotalOverall(unitsCount, cancellationToken);
             await AboutTotalAtBrands(unitsCount, cancellationToken);
             await AboutTotalAtCountries(unitsCount, cancellationToken);
+
+            await UpdateSnapshot(unitsCountSnapshot, unitsCount, cancellationToken);
         }
         catch (Exception e)
         {
             _log.LogError(e, "Can't check and notify units count");
         }
+    }
+
+    private async Task UpdateSnapshot<TData>(Snapshot<TData> oldSnapshot, TData data, CancellationToken cancellationToken)
+    {
+        var newSnapshot = Snapshot<TData>.Create(oldSnapshot, data);
+
+        await _snapshotsRepository.Save(newSnapshot, cancellationToken);
     }
 
     private async Task AboutTotalOverall(BrandListTotalUnitCountListModel unitsCount, CancellationToken cancellationToken)
