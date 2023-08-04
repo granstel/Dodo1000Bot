f0254d35aaa103010e4e8f8d82b231594f53e786
Stepan Grankin <sgrankin@inbox.ru>
2023-08-04 09:39:04 +0300

    removed unused property

--- Dodo1000Bot.Models/GlobalApi/ResultListModel.cs
+++ Dodo1000Bot.Models/GlobalApi/ResultListModel.cs
@@ -5,7 +5,5 @@ namespace Dodo1000Bot.Models.GlobalApi
     public class ResultListModel<T>
     {
         public IEnumerable<T> Countries { get; set; }
-
-        public IEnumerable<ErrorModel> Errors { get; set; }
     }
 }
