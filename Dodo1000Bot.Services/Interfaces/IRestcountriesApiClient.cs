41cb4b6641c78bfb596ec12816b6c86aad4fe364
Stepan Grankin <sgrankin@inbox.ru>
2023-08-06 18:48:29 +0300

    RestcountriesApiClient

---
+++ Dodo1000Bot.Services/Interfaces/IRestcountriesApiClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Dodo1000Bot.Models.Restcountries;
+
+namespace Dodo1000Bot.Services;
+
+public interface IRestcountriesApiClient
+{
+    Task<Country[]> GetNames(CancellationToken cancellationToken, params string[] codes);
+}
