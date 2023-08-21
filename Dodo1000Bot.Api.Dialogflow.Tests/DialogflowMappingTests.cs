5fab5762ac90fc9edc679be9b0f49d3b59dd822a
Stepan Grankin <sgrankin@inbox.ru>
2023-08-21 09:37:29 +0300

    added Dialogflow tests

---
+++ Dodo1000Bot.Api.Dialogflow.Tests/DialogflowMappingTests.cs
@@ -0,0 +1,24 @@
+﻿using AutoFixture;
+using AutoMapper;
+using NUnit.Framework;
+
+namespace Dodo1000Bot.Api.Dialogflow.Tests
+{
+    [TestFixture]
+    public class DialogflowMappingTests
+    {
+        private IMapper _target;
+
+        [SetUp]
+        public void InitTest()
+        {
+            _target = new Mapper(new MapperConfiguration(c => c.AddProfile<DialogflowMapping>()));
+        }
+
+        [Test]
+        public void ValidateConfiguration_Success()
+        {
+            _target.ConfigurationProvider.AssertConfigurationIsValid();
+        }
+    }
+}
