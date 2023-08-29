e77715577cfc6976383050eef018625d22e9c528
Stepan Grankin <sgrankin@inbox.ru>
2023-08-29 10:19:05 +0300

    DROP FOREIGN KEY pushed_notifications_userId_users_Id

---
+++ Dodo1000Bot.Api/Migrations/202308290945_DropForeignKeyToUserId.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace Dodo1000Bot.Api.Migrations;
+
+[Migration(202308290945)]
+public class DropForeignKeyToUserId: Migration
+{
+    public override void Up()
+    {
+        Execute.Sql
+        (@"
+            ALTER TABLE pushed_notifications 
+            DROP FOREIGN KEY `pushed_notifications_userId_users_Id`;
+        ");
+    }
+
+    public override void Down()
+    {
+        throw new System.NotImplementedException();
+    }
+}
