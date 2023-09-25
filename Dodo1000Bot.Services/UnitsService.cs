1cc3f1a93b9062174834e2ffc8baf67a8619285c
Stepan Grankin <sgrankin@inbox.ru>
2023-09-25 16:12:42 +0300

    AboutTotalCountriesAtBrands

--- Dodo1000Bot.Services/UnitsService.cs
+++ Dodo1000Bot.Services/UnitsService.cs
@@ -46,6 +46,7 @@ public class UnitsService : CheckAndNotifyService
             await AboutTotalOverall(unitsCount, cancellationToken);
             await AboutTotalAtBrands(unitsCount, cancellationToken);
             await AboutTotalAtCountries(unitsCount, cancellationToken);
+            await AboutTotalCountriesAtBrands(unitsCount, cancellationToken);
 
             var snapshotName = nameof(_globalApiClient.UnitsCount);
             var unitsCountSnapshot = 
@@ -211,6 +212,32 @@ public class UnitsService : CheckAndNotifyService
             {
                 await CheckAndNotify1000(totalAtCountry, brand, cancellationToken);
             }
+        }
+    }
+
+    internal async Task AboutTotalCountriesAtBrands(BrandListTotalUnitCountListModel unitsCount, CancellationToken cancellationToken)
+    {
+        var countriesCountAtBrands = unitsCount.Brands
+            .ToDictionary(b => b.Brand, b => b.Countries.Count());
+
+        foreach (var countriesCountAtBrand in countriesCountAtBrands)
+        {
+            var brand = countriesCountAtBrand.Key;
+
+            if (!CheckHelper.CheckRemainder(countriesCountAtBrand.Value, 10))
+            {
+                continue;
+            }
+
+            var notification = new Notification
+            {
+                Payload = new NotificationPayload
+                {
+                    Text = $"🌏 Awesome! {brand} is already in {countriesCountAtBrand.Value} countries!"
+                }
+            };
+
+            await _notificationsService.Save(notification, cancellationToken);
         }
     }
 
