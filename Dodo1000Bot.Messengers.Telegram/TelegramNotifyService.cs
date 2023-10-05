9173ef4a0716620a665fc09f01b287bb663df51f
Stepan Grankin <sgrankin@inbox.ru>
2023-10-05 18:33:04 +0300

    delete user on forbidden exception

--- Dodo1000Bot.Messengers.Telegram/TelegramNotifyService.cs
+++ Dodo1000Bot.Messengers.Telegram/TelegramNotifyService.cs
@@ -8,19 +8,20 @@ using Dodo1000Bot.Models.Domain;
 using Dodo1000Bot.Services;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
+using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types.Enums;
 
 namespace Dodo1000Bot.Messengers.Telegram;
 
 public class TelegramNotifyService: INotifyService
 {
-    private readonly IUsersRepository _usersRepository;
+    private readonly IUsersService _usersService;
     private readonly ITelegramBotClient _client;
     private readonly ILogger<TelegramNotifyService> _log;
 
-    public TelegramNotifyService(IUsersRepository usersRepository, ITelegramBotClient client, ILogger<TelegramNotifyService> log)
+    public TelegramNotifyService(IUsersService usersService, ITelegramBotClient client, ILogger<TelegramNotifyService> log)
     {
-        _usersRepository = usersRepository;
+        _usersService = usersService;
         _client = client;
         _log = log;
     }
@@ -35,7 +36,7 @@ public class TelegramNotifyService: INotifyService
             return pushedNotifications;
         }
 
-        IEnumerable<User> users = await _usersRepository.GetUsers(Source.Telegram, cancellationToken);
+        IEnumerable<User> users = await _usersService.GetUsers(Source.Telegram, cancellationToken);
 
         if (users?.Any() != true)
         {
@@ -44,7 +45,7 @@ public class TelegramNotifyService: INotifyService
 
         foreach (var user in users)
         {
-            var pushedNotificationsToUsers = await PushNotificationsToUsers(notifications, user, cancellationToken);
+            var pushedNotificationsToUsers = await PushNotificationsToUser(notifications, user, cancellationToken);
 
             pushedNotifications.AddRange(pushedNotificationsToUsers);
         }
@@ -52,7 +53,7 @@ public class TelegramNotifyService: INotifyService
         return pushedNotifications;
     }
 
-    private async Task<IList<PushedNotification>> PushNotificationsToUsers(IList<Notification> notifications, User user, CancellationToken cancellationToken)
+    private async Task<IList<PushedNotification>> PushNotificationsToUser(IList<Notification> notifications, User user, CancellationToken cancellationToken)
     {
         var pushedNotifications = new List<PushedNotification>();
 
@@ -61,15 +62,22 @@ public class TelegramNotifyService: INotifyService
             var messengerUserId = user.MessengerUserId;
             try
             {
-                await _client.SendTextMessageAsync(messengerUserId, notification.Payload.Text, parseMode: ParseMode.Html,
+                await _client.SendTextMessageAsync(messengerUserId, notification.Payload.Text,
+                    parseMode: ParseMode.Html,
                     cancellationToken: cancellationToken);
 
                 var coordinates = notification.Payload.Coordinates;
                 if (coordinates != null)
                 {
-                    await _client.SendLocationAsync(messengerUserId, coordinates.Latitude, coordinates.Longitude, 
+                    await _client.SendLocationAsync(messengerUserId, coordinates.Latitude, coordinates.Longitude,
                         cancellationToken: cancellationToken);
                 }
+            }
+            catch (ApiRequestException e) when (e.ErrorCode == 403)
+            {
+                _log.LogWarning(e, "Error while send notification to {MessengerUserId}, so user will be deleted", messengerUserId);
+                await _usersService.Delete(user, cancellationToken);
+                return pushedNotifications;
             }
             catch (Exception e)
             {
