874f0afa84d622df0fded3849ae70c4afd0d283b
Stepan Grankin <sgrankin@inbox.ru>
2023-10-22 18:04:45 +0300

    AddColumn_notifications_Type migration

---
+++ Dodo1000Bot.Api/Migrations/202310221801_AddColumn_notifications_Type.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace Dodo1000Bot.Api.Migrations;
+
+[Migration(202310221801)]
+public class AddColumn_notifications_Type: Migration
+{
+    public override void Up()
+    {
+        Execute.Sql
+        (@"
+            ALTER TABLE notifications 
+            ADD COLUMN `Type` TINYINT(2) NOT NULL DEFAULT 0,
+            ADD INDEX `Type` (`Type` ASC) VISIBLE;
+        ");
+    }
+
+    public override void Down()
+    {
+        throw new System.NotImplementedException();
+    }
+}
