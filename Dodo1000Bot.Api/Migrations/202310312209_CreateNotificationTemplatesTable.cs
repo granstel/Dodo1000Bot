a4a72c74df951a15f5e4365fe71efaa1dfa25cd3
Stepan Grankin <sgrankin@inbox.ru>
2023-10-31 22:18:39 +0300

    CreateNotificationTemplatesTable migration

---
+++ Dodo1000Bot.Api/Migrations/202310312209_CreateNotificationTemplatesTable.cs
@@ -0,0 +1,26 @@
+﻿using FluentMigrator;
+
+namespace Dodo1000Bot.Api.Migrations;
+
+[Migration(202310312209)]
+public class CreateNotificationTemplatesTable: Migration
+{
+    public override void Up()
+    {
+        Execute.Sql
+        (@"
+            CREATE TABLE notification_templates (
+              `Id` INT NOT NULL AUTO_INCREMENT,
+              `NotificationType` TINYINT(2) NOT NULL,
+              `MessengerType` TINYINT(1) NOT NULL,
+              `LanguageCode` VARCHAR(2) NOT NULL,
+              `Template` VARCHAR(450) NOT NULL,
+              PRIMARY KEY (`Id`));
+        ");
+    }
+
+    public override void Down()
+    {
+        throw new System.NotImplementedException();
+    }
+}
