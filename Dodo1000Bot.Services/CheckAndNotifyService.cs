9648e1690a24509a610ee278d436ceec0cc81983
Stepan Grankin <sgrankin@inbox.ru>
2023-12-14 09:55:20 +0300

    revert disposed CheckAndNotifyService

--- Dodo1000Bot.Services/CheckAndNotifyService.cs
+++ Dodo1000Bot.Services/CheckAndNotifyService.cs
@@ -1,19 +1,10 @@
-using System;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Dodo1000Bot.Services
 {
-    public abstract class CheckAndNotifyService : IAsyncDisposable
+    public abstract class CheckAndNotifyService
     {
         public abstract Task CheckAndNotify(CancellationToken cancellationToken);
-
-        protected abstract ValueTask DisposeAsyncCore();
-
-        public async ValueTask DisposeAsync()
-        {
-            await DisposeAsyncCore();
-            GC.SuppressFinalize(this);
-        }
     }
 }
