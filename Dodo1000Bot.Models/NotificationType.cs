eacf283fbb0c66895d9fea0626946625a1dc656f
Stepan Grankin <sgrankin@inbox.ru>
2023-12-20 09:05:51 +0300

    numbers at NotificationType

--- Dodo1000Bot.Models/NotificationType.cs
+++ Dodo1000Bot.Models/NotificationType.cs
@@ -2,16 +2,16 @@
 
 public enum NotificationType
 {
-    Custom,
-    TotalOverall,
-    TotalAtBrands,
-    TotalAtCountries,
-    TotalCountriesAtBrands,
-    NewCountry,
-    NewUnit,
-    OrdersPerMinute,
-    YearRevenue,
-    SubscribersCount,
-    Admin,
-    Emoji,
+    Custom = 0,
+    TotalOverall = 1,
+    TotalAtBrands = 2,
+    TotalAtCountries = 3,
+    TotalCountriesAtBrands = 4,
+    NewCountry = 5,
+    NewUnit = 6,
+    OrdersPerMinute = 7,
+    YearRevenue = 8,
+    SubscribersCount = 9,
+    Admin = 10,
+    Emoji = 11,
 }
