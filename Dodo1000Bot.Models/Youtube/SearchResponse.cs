d53c0afc281d842dad367c444dceeb1803e92d26
Stepan Grankin <sgrankin@inbox.ru>
2024-02-12 09:32:47 +0300

    youtube models

---
+++ Dodo1000Bot.Models/Youtube/SearchResponse.cs
@@ -0,0 +1,24 @@
+﻿namespace Dodo1000Bot.Models.Youtube;
+
+public class SearchResponse
+{
+    public Items[] Items { get; set; }
+}
+
+public class Items
+{
+    public Id Id { get; set; }
+    public Snippet Snippet { get; set; }
+}
+
+public class Id
+{
+    public string VideoId { get; set; }
+}
+
+public class Snippet
+{
+    public string PublishedAt { get; set; }
+    public string LiveBroadcastContent { get; set; }
+    public string PublishTime { get; set; }
+}
