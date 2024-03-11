22ea6dd5d85a5e5ab45da8dbd9c452fbd8d08430
Stepan Grankin <sgrankin@inbox.ru>
2024-03-11 08:59:57 +0300

    MetricsCollector

---
+++ Dodo1000Bot.Services/Metrics/MetricsCollector.cs
@@ -0,0 +1,19 @@
+﻿using Prometheus;
+
+namespace Dodo1000Bot.Services.Metrics;
+
+public static class MetricsCollector
+{
+    private static readonly Gauge Metrics;
+
+    static MetricsCollector()
+    {
+        Metrics = Prometheus.Metrics
+            .CreateGauge("metrics", "Custom metrics", "metric_name", "parameter");
+    }
+
+    public static void Increment(string key, string value)
+    {
+        Metrics.WithLabels(key, value).Inc();
+    }
+}
