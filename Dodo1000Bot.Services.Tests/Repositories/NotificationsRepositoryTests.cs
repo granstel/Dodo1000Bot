118265a23e077ed3413b2dd58df117fcb2e14919
Stepan Grankin <sgrankin@inbox.ru>
2024-08-28 22:49:28 +0300

    fixed tests

--- Dodo1000Bot.Services.Tests/Repositories/NotificationsRepositoryTests.cs
+++ Dodo1000Bot.Services.Tests/Repositories/NotificationsRepositoryTests.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using AutoFixture;
 using Dodo1000Bot.Models.Domain;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
 using MySql.Data.MySqlClient;
 using NUnit.Framework;
 
@@ -12,6 +14,7 @@ namespace Dodo1000Bot.Services.Tests.Repositories;
 
 public class NotificationsRepositoryTests
 {
+    private ILogger<NotificationsRepository> _logger;
     private MySqlConnection _connection;
 
     private NotificationsRepository _target;
@@ -21,6 +24,7 @@ public class NotificationsRepositoryTests
     [SetUp]
     public void SetUp()
     {
+        _logger = Mock.Of<ILogger<NotificationsRepository>>();
         var connectionString = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddUserSecrets<NotificationsRepositoryTests>()
@@ -29,7 +33,7 @@ public class NotificationsRepositoryTests
 
         _connection = new MySqlConnection(connectionString);
 
-        _target = new NotificationsRepository(_connection);
+        _target = new NotificationsRepository(_logger, _connection);
 
         _fixture = new Fixture { OmitAutoProperties = true };
     }
